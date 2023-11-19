f6edd71040edb7d0d0ab774e513f627aab2a7fe2
Ilya Mokin <ilia.mokin@gmail.com>
2023-11-19 20:09:19 +0100

    model = model.to_bettertransformer()

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 model_id = "openai/whisper-large-v3"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True, use_flash_attention_2=True
+    model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
 )
+model = model.to_bettertransformer()
 model.to(device)
 
 processor = AutoProcessor.from_pretrained(model_id)
